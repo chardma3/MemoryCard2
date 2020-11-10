b39944403a2c1529140438abb351443ee905930f
chardma3 <chardma3@hotmail.com>
2020-11-10 13:03:35 +0000

    added js flipping

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.add('flip');
+
+
+if (!hasFlippedCard) {
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+} else {
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    //do catds match?
+    
+
+    console.log({firstCard, secondCard});
+    }
+}
+cards.forEach(card => card.addEventListener('click', flipCard))
+;
